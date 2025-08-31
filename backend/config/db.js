9fdbd2d95a67618d6fc7790487c5a14016b71281
Souvik0001 <souvikchowdhury095@gmail.com>
2025-08-31 20:28:48 +0530

    Security Update...

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,6 +2,6 @@ import mongoose from "mongoose";
 
 export const  connectDB = async () =>{
 
-    await mongoose.connect('mongodb+srv://VelvetBug:1L0veBiriy*ni@cluster0.5febq.mongodb.net/VelvetBugMain').then(()=>console.log("DB Connected"));
+    await mongoose.connect(process.env.MONGO_URL).then(()=>console.log("DB Connected"));
    
 }
